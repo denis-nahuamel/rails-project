a7a2be797d21ae5a209d45763895fc321ad86a2b
denis-nahuamel <nahuamel.denis@gmail.com>
2022-08-13 03:59:50 +0000

    get properties with style

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,16 +1,6 @@
 class PropertiesController < ApplicationController
   def index
     client = EasyBroker.client
-    # client.properties.find('EB-123')
-    # client.properties.find('EB-123')
-    client.properties
-    @properties = client.properties.search # results is an enumerator
-
-
-    # @properties = client.properties # results is an enumerator
-    puts "******************"
-    p @properties
-    # @properties = client.properties.search(search: { statuses: [:published] }, limit: 10, page: 1) # results is an enumerator
-    # @properties = results.total 
+    @properties = client.properties.search
   end
 end
